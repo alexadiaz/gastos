a3dfcaf6f1dc396cec0480c85ec793f0b4b14628
Alexa <alexadiaz@msn.com>
2017-09-21 20:05:09 -0400

    funcion borrar gastos y validar si el gasto existe en la base de datos

--- gastos.js
+++ gastos.js
@@ -15,14 +15,39 @@ const db = pgp({
 });
 
 let gastos = {
-    insertar: insertarGastos()
+    insertar: insertarGastos,
+    borrar: borrarGastos()
 };
 
 function insertarGastos(){
-    rl.question("Ingrese gasto ",nombreGasto =>{
+    rl.question("Ingrese gasto: ",nombreGasto =>{
         db.none(`insert into gastos (nombre) values ('${nombreGasto}')`)
         .then(()=> console.log("Gasto insertado"))
-        .catch(error => console.log("El nombre ya existe"));
+        .catch(error => console.log("El gasto ya existe"));
+    });
+}
+
+function borrarGastos(){
+    rl.question("Ingrese gasto: ",nombreGasto =>{
+        isGuardado(nombreGasto)
+        .then(result => {
+            if (result){
+                return db.none(`delete from gastos where nombre = '${nombreGasto}'`)
+                .then(()=> {return "Gasto borrado";});
+            }
+            return "El gasto no existe";
+        }).then(mensaje =>{
+            console.log(mensaje);
+            rl.close();
+        });
+    });
+}
+
+function isGuardado(nombreGasto){
+    return new Promise(resolve =>{
+        db.one(`select nombre from gastos where nombre = '${nombreGasto}'`)
+        .then(()=> resolve (true))
+        .catch(()=> resolve(false));
     });
 }
 
