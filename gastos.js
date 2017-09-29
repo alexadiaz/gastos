356b48b2c372bbc0ae1a882232dda5db8afe9fa3
Alexa <alexadiaz@msn.com>
2017-09-28 22:53:49 -0400

    archivo sin captura de datos

--- gastos.js
+++ gastos.js
@@ -1,10 +1,4 @@
-const readline = require("readline");
 const pgp = require("pg-promise")();
-
-const rl = readline.createInterface({
-    input:process.stdin,
-    output:process.stdout
-});
 
 const db = pgp({
         host:"localhost",
@@ -14,326 +8,245 @@ const db = pgp({
         password:"postgres"
 });
 
+let gastos ={
+    tabla:"gastos",
+    tablaConsultar: "pagosrealizados",
+    campo:"gastosid"
+};
+
+let ingresos ={
+    tabla: "ingresos",
+    tablaConsultar: "pagosrecibidos",
+    campo:"ingresoid"
+};
+
 let controlGastos = {
     gastos:{
-        insertar: () => insertar("gastos"),
-        borrar: () => borrar("gastos"),
-        renombrar: () => renombrar("gastos"),
+        insertar: (datos) => insertar("gastos",datos),
+        borrar: (datos) => borrar("gastos",datos),
+        renombrar: (datos) => renombrar(gastos,datos),
         consultar: () => consultar("gastos")
     },
     ingresos:{
-        insertar: () => insertar("ingresos"),
-        borrar: () => borrar("ingresos"),
-        renombrar: () => renombrar("ingresos"),
+        insertar: (datos) => insertar("ingresos",datos),
+        borrar: (datos) => borrar("ingresos",datos),
+        renombrar: (datos) => renombrar(ingresos,datos),
         consultar: () => consultar("ingresos")
     },
     periodos:{
-        insertar:() => insertarPeriodos(),
-        borrar: () => borrarPeriodos(),
-        renombrar: () => renombrarPeriodos(),
+        insertar:(datos) => insertarPeriodos(datos),
+        borrar: (datos) => borrarPeriodos(datos),
+        renombrar: (datos) => renombrarPeriodos(datos),
         consultar: () => consultarPeriodos()
     },
     pagosrecibidos:{
-        insertar: () => insertarPagos("pagosrecibidos","ingresos","ingresoid"),
-        borrar: () => borrarPagos("pagosrecibidos"),
-        renombrar: () => renombrarPagos("pagosrecibidos","ingresos","ingresoid"),
+        insertar: (datos) => insertarPagos(ingresos,datos),
+        borrar: (datos) => borrarPagos("pagosrecibidos",datos),
+        renombrar: (datos) => renombrarPagos(ingresos,datos),
         consultar: () => consultarPagosRecibidos()
     },
     pagosrealizados:{
-        insertar: () => insertarPagos("pagosrealizados","gastos","gastosid"),
-        borrar: () => borrarPagos("pagosrealizados"),
-        renombrar: () => renombrarPagos("pagosrealizados","gastos","gastosid"),
+        insertar: (datos) => insertarPagos(gastos,datos),
+        borrar: (datos) => borrarPagos("pagosrealizados",datos),
+        renombrar: (datos) => renombrarPagos(gastos,datos),
         consultar: () => consultarPagosRealizados()
     }
 };
 
-function insertar(tabla){
-    rl.question("Ingrese nombre: ",nombre =>{
-        if (nombre !== ""){
-            db.none(`insert into ${tabla} (nombre) values ('${nombre}')`)
+function insertar(tabla,datos){
+    if (datos.nombre !== ""){
+        db.none(`insert into ${tabla} (nombre) values ($1)`,datos.nombre)
             .then(()=> console.log("Nombre insertado"))
             .catch(() => console.log("Nombre ya existe"));
-        }
-    });
+    }
+    else{
+        console.log("Debe ingresar un nombre");
+    }
 }
 
-function borrar(tabla){
-    rl.question("Ingrese nombre: ",nombre =>{
-        isGuardado(tabla,nombre)
-        .then(id => {
-            if (!id){
-                return "Nombre no existe";
-            }
-            return db.none(`delete from ${tabla} where nombre = '${nombre}'`)
-            .then(()=> {return "Nombre borrado";})
-            .catch(() => {return "Nombre esta siendo usado en pagos realizados y/o recibidos";});
-        }).then(mensaje =>{
-            console.log(mensaje);
-            rl.close();
-        });
-    });
+function borrar(tabla,datos){
+    db.one(`select id from ${tabla} where nombre = $1`, datos.nombre)
+        .then(() => {
+            db.none(`delete from ${tabla} where nombre = $1`, datos.nombre)
+                .then(()=> console.log("Nombre borrado"))
+                .catch(() => console.log ("Nombre esta siendo usado en pagos realizados y/o recibidos"));
+        })
+        .catch (() => console.log("Nombre no existe"));
 }
 
-function renombrar(tabla){
-    rl.question("Ingrese nombre: ",nombre =>{
-        isGuardado(tabla,nombre)
-        .then(id =>{
-            if(!id){
-                return "Nombre no existe";
-            }
-            return isGuardadoPagos(tabla,id)
-            .then(result =>{
-                if(result.length !== 0){
+function renombrar(info,datos){
+    if (datos.nuevoNombre !== ""){
+        db.one(`select id from ${info.tabla} where nombre = $[nombre]`, datos)
+            .then((id) =>{
+                return db.one(`select count(id) from ${info.tablaConsultar} where ${info.campo} = $1`,id.id, c => parseInt(c.count, 10));
+            })
+            .then(count =>{
+                if(count !== 0){
                     return "Nombre esta siendo usado en pagos realizados y/o recibidos";
                 }
-                return nuevoRenombrar(tabla,nombre);
-            });
-        }).then(mensaje =>{
-            console.log(mensaje);
-            rl.close();
-        });
-    });
-}
-
-function nuevoRenombrar(tabla,nombre){
-    return new Promise(resolve =>{
-        rl.question("Ingrese nuevo nombre: ",nuevoNombre =>{
-            if(nuevoNombre !== ""){
-                db.none(`update ${tabla} set nombre = '${nuevoNombre}' where nombre = '${nombre}'`)
-                .then(()=> resolve ("Nombre renombrado"))
-                .catch(()=> resolve ("Nombre ya existe"));
-            }
-        });
-    });
+                return db.none(`update ${info.tabla} set nombre = $[nuevoNombre] where nombre = $[nombre]`, datos)
+                        .then(()=> "Nombre renombrado")
+                        .catch(()=> "Nombre ya existe");
+            }).then(mensaje => console.log(mensaje))
+            .catch (() => console.log("Nombre no existe"));
+    }
+    else{
+        console.log("Debe ingresar nuevo nombre");
+    }
 }
 
 function consultar(tabla){
     db.any(`select nombre from ${tabla}`)
-    .then(result => console.log(result));
+        .then(result => console.log(result));
 }
 
-function isGuardado(tabla,nombre){
-    return db.one(`select id from ${tabla} where nombre = '${nombre}'`)
-        .then((id)=> id)
-        .catch(()=> false);
-}
-
-function isGuardadoPagos(tabla,id){
-    let tablaConsultar = null;
-    let campo = null;
-    if(tabla === "gastos"){
-        tablaConsultar = "pagosrealizados";
-        campo = "gastosid";
-    }
-    else if(tabla === "ingresos"){
-        tablaConsultar = "pagosrecibidos";
-        campo = "ingresoid"; 
-    }
-    else if(tabla === "pagosrecibidos"){
-        tablaConsultar = tabla;
-        campo =  "periodoid";
-    }
-    else{
-        tablaConsultar = tabla;
-        campo =  "periodoid";
-    }
-    return db.any(`select id from ${tablaConsultar} where ${campo} = ${id.id}`)
-    .then((result)=> result);
-}
-
-function insertarPeriodos(){
-    periodos()
-    .then(datos =>{
-        if(datos){
-            db.none(`insert into periodos (mes,ano) values (${datos.mes},${datos.ano})`)
+function insertarPeriodos(datos){
+    if (datos.mes !== "" && datos.ano !== ""){
+        db.none("insert into periodos (mes,ano) values ($[mes],$[ano])",datos)
             .then(()=> console.log("Periodo ingresado"))
             .catch(()=> console.log("Periodo no valido o ya existe"));
-        }
-    });
+    }
+    else{
+        console.log("Debe ingresar mes y ano");
+    }
 }
 
-function borrarPeriodos(){
-    periodos()
-    .then(datos =>{
-        if(datos){
-            isGuardadoPeriodo(datos.mes,datos.ano)
-            .then(result =>{
-                if (result){
-                    return db.none(`delete from periodos where mes = ${datos.mes} and ano = ${datos.ano}`)
-                    .then(()=> {return "Periodo borrado";})
-                    .catch(() => {return "Periodo esta siendo usado en pagos realizados y/o recibidos";});
-                }
-                return "Periodo no existe";
-            }).then(mensaje =>{
-                console.log(mensaje);
-                rl.close();
-            });
-        }
-    });
+function borrarPeriodos(datos){
+    db.one("select id from periodos where mes=$[mes] and ano=$[ano]",datos)
+        .then(() =>{
+            db.none("delete from periodos where mes = $[mes] and ano = $[ano]",datos)
+                .then(()=> console.log ("Periodo borrado"))
+                .catch(() => console.log ("Periodo esta siendo usado en pagos realizados y/o recibidos"));
+        })
+        .catch(() => console.log("Periodo no existe"));
 }
 
-function renombrarPeriodos(){
-   periodos()
-   .then(datos =>{
-       if(datos){
-            isGuardadoPeriodo(datos.mes,datos.ano)
-            .then(id =>{
-                if(!id){
-                    return "Periodo no existe";
+function renombrarPeriodos(datos){
+    if(datos.nuevoMes !== "" && datos.nuevoAno !== ""){
+        db.one("select id from periodos where mes=$[mes] and ano=$[ano]",datos)
+            .then((id) =>{
+                let q1 = db.one("select count(id) from pagosrecibidos where periodoid = $1", id.id, c => parseInt(c.count,10));
+                let q2 = db.one("select count(id) from pagosrealizados where periodoid = $1",id.id, c => parseInt(c.count,10));
+                return Promise.all([q1, q2]);
+            })
+            .then(result => {
+                if(result[0] !== 0){
+                return "Periodo esta siendo usado en pagos recibidos";
                 }
-                return isGuardadoPagos("pagosrecibidos",id)
-                .then(result =>{
-                    if(result.length !== 0){
-                        return "Periodo esta siendo usado en pagos recibidos";
-                    }
-                    return isGuardadoPagos("pagosrealizados",id)
-                    .then(result => {
-                        if(result.length !== 0){
-                            return "Periodo esta siendo usado en pagos realizados";
-                        }
-                        return nuevoRenombrarPeriodos(datos.mes,datos.ano);
-                    });
-                });
-            }).then(mensaje =>{
-                console.log(mensaje);
-                rl.close();
-            });
-        }
-    });
-}            
+                if(result[1] !== 0){
+                    return "Periodo esta siendo usado en pagos realizados";
+                }
+                return db.none("update periodos set mes = $[nuevoMes],ano = $[nuevoAno] where mes = $[mes] and ano = $[ano]",datos)
+                        .then(()=> "Periodo renombrado")
+                        .catch(()=> "Periodo ya existe");
+            })     
+            .then(mensaje => console.log(mensaje))
+            .catch(() => console.log("Periodo no existe"));
+    }
+    else{
+        console.log("Debe ingresar nuevo mes y nuevo ano");
+    }
+}
                     
-function nuevoRenombrarPeriodos(mes,ano){
-    return new Promise(resolve =>{
-        periodos()
-        .then(datos =>{
-            if(datos){
-                db.none(`update periodos set mes = ${datos.mes},ano = ${datos.ano} where mes = ${mes} and ano = ${ano}`)
-                .then(()=> resolve ("Periodo renombrado"))
-                .catch(()=> resolve ("Periodo ya existe"));
-            }
-        });
-    });
-}
-       
 function consultarPeriodos(){
     db.any("select mes,ano from periodos")
-    .then(result => console.log(result));
+        .then(result => console.log(result));
 }
 
-function periodos(){
-    return new Promise (resolve => {
-        let datos = null;
-        rl.question("Ingrese mes: ",mes =>{
-            if (mes !== ""){
-                rl.question("Ingrese ano: ",ano =>{
-                    if(ano !== ""){
-                        resolve (datos = {mes: mes,ano: ano});
-                        return;
-                    }
-                    resolve (false);
-                });
-            }
-        });
-    });
-}
-
-function isGuardadoPeriodo(mes,ano){
-     return db.one(`select id from periodos where mes=${mes} and ano=${ano}`)
-    .then((id) => id)
-    .catch(() => false);
-}
-
-function insertarPagos(tabla,tablaCompara,campo){
-    periodos()
-    .then(datos =>{
-        if(datos){
-            isGuardadoPeriodo(datos.mes,datos.ano)
-            .then(periodoid =>{
-                if (!periodoid){
-                    return "Periodo no existe";
+function insertarPagos(info,datos){
+    if (datos.mes !== "" && datos.ano !== "" && datos.nombre !== "" && datos.valor !== ""){
+        pagos(info,datos)
+            .then(result =>{
+                if (result){
+                    return db.none(`insert into ${info.tablaConsultar} (periodoid,${info.campo},valor) values ($1,$2,$3)`,[result[0].id,result[1].id,datos.valor])
+                        .then(()=> "Pago ingresado")
+                        .catch(()=> "Valor no es valido");
                 }
-                return pagos(tabla,tablaCompara,campo,periodoid,"insertar");
-            }).then(mensaje =>{
-                console.log(mensaje);
-                rl.close();
-            });
-        }
-    });
+                return result;
+            })
+            .then(mensaje => console.log(mensaje));
+    }
+    else{
+        console.log("Debe ingresar mes, ano, nombre y valor");
+    }
 }
 
-function pagos(tabla,tablaCompara,campo,periodoid,accion,idRenombrar){
-    return new Promise(resolve =>{
-        rl.question("Ingrese nombre: ",nombre =>{
-            if (nombre !== ""){
-                isGuardado(tablaCompara,nombre)
-                .then(id =>{
-                    if (!id){
-                        resolve ("Nombre no existe");
-                        return;
-                    }
-                    rl.question("Ingrese valor: ",valor =>{
-                        if (valor !== ""){
-                            if(accion === "insertar"){
-                                db.none(`insert into ${tabla} (periodoid,${campo},valor) values (${periodoid.id},${id.id},${valor})`)
-                                .then(()=> resolve ("Pago ingresado"))
-                                .catch(()=> resolve ("Valor no es valido"));
-                            }
-                            else{
-                                db.none(`update ${tabla} set periodoid = ${periodoid.id},${campo} = ${id.id},valor = ${valor} where id = ${idRenombrar}`)
-                                .then(()=> resolve ("Pago renombrado"))
-                                .catch(()=> resolve ("Valor no es valido"));
-                            }
-                        }
-                     });
-                });
+function pagos(info,datos){
+    let q1 = db.oneOrNone("select id from periodos where mes=$[mes] and ano=$[ano]",datos);
+    let q2 = db.oneOrNone(`select id from ${info.tabla} where nombre = $1`, datos.nombre);
+    return Promise.all([q1,q2])
+        .then(result =>{
+            if(result[0] === null){
+                return "Periodo no existe";
             }
+            if(result[1] === null){
+                return "Nombre no existe";
+            }
+            return true;
         });
-    });
 }
 
-function borrarPagos(tabla){
-    rl.question("Ingrese id: ",id =>{
-        db.one(`select id from ${tabla} where id = ${id}`)
-        .then(result => {
-            db.none(`delete from ${tabla} where id = ${result.id}`)
-            .then(()=> console.log ("Pago borrado"));
+function borrarPagos(tabla,datos){
+    db.one(`select id from ${tabla} where id = $1`,datos.id)
+        .then(id => {
+            return db.none(`delete from ${tabla} where id = $1`,id.id);
         })
+        .then(()=> console.log ("Pago borrado"))
         .catch(()=> console.log("id no existe"));
-    });
 }
 
-function renombrarPagos(tabla,tablaCompara,campo){
-    rl.question("Ingrese id: ",id =>{
-        db.one(`select id from ${tabla} where id = ${id}`)
-        .then(result => {
-            periodos()
-            .then(datos =>{
-                if (datos){
-                    isGuardadoPeriodo(datos.mes,datos.ano)
+function renombrarPagos(info,datos){
+    if(datos.id !== "" && datos.mes !== "" && datos.ano !== "" && datos.nombre !== "" && datos.valor !== ""){
+        db.one(`select id from ${info.tablaConsultar} where id = $1`,datos.id)
+            .then(() => {
+                return pagos(info,datos);
+            })
+            .then(result =>{
+                if (result){
+                    return db.none(`update ${info.tablaConsultar} set periodoid = $1,${info.campo} = $2,valor = $3 where id = $4`,[result[0].id,result[1].id,datos.valor,datos.id])
+                            .then(()=> "Pago renombrado")
+                            .catch(()=> "Valor no es valido");
+                }
+                return result;
+            }).then(mensaje => console.log(mensaje))
+            .catch(()=> console.log("id no existe"));
+    }
+    else{
+        console.log("Debe ingresar id, mes, ano, nombre y valor");
+    }
+}
+
+function renombrarPagos(info,datos){
+    if(datos.id !== "" && datos.mes !== "" && datos.ano !== "" && datos.nombre !== "" && datos.valor !== ""){
+        db.one(`select id from ${info.tablaConsultar} where id = $1`,datos.id)
+            .then(idRenombrar => {
+                db.one("select id from periodos where mes=$[mes] and ano=$[ano]",datos)
                     .then(periodoid =>{
-                        if (!periodoid){
-                            return "Periodo no existe";
-                        }
-                        return pagos(tabla,tablaCompara,campo,periodoid,"renombrar",id);
-                    }).then(mensaje =>{
-                        console.log(mensaje);
-                        rl.close();
-                    });
-                }
-            });
-        })
-        .catch(()=> console.log("id no existe"));
-    });
+                        db.one(`select id from ${info.tabla} where nombre = $1`, datos.nombre)
+                            .then((id) =>{
+                                db.none(`update ${info.tablaConsultar} set periodoid = $1,${info.campo} = $2,valor = $3 where id = $4`,[periodoid.id,id.id,datos.valor,idRenombrar.id])
+                                    .then(()=> "Pago renombrado")
+                                    .catch(()=> "Valor no es valido");
+                            })
+                            .catch(() => console.log("Nombre no existe"));
+                    }).then(mensaje => console.log(mensaje))
+                    .catch(()=> console.log("Periodo no existe"));
+            })
+            .catch(()=> console.log("id no existe"));
+    }
+    else{
+        console.log("Debe ingresar id, mes, ano, nombre y valor");
+    }
 }
 
 function consultarPagosRecibidos(){
     db.any("select pr.id,pe.mes,pe.ano,i.nombre,pr.valor from pagosrecibidos pr join periodos pe on pr.periodoid= pe.id join ingresos i on pr.ingresoid=i.id")
-    .then(result => console.log(result));
+        .then(result => console.log(result));
 }
 
 function consultarPagosRealizados(){
     db.any("select pr.id,pe.mes,pe.ano,ga.nombre,pr.valor from pagosrealizados pr join periodos pe on pr.periodoid= pe.id join gastos ga on pr.gastosid=ga.id")
-    .then(result => console.log(result));
+        .then(result => console.log(result));
 }
 
 module.exports = controlGastos;
